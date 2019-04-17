a55f014fa714eb5e9e038a45bfa6d4a98b87c68b
benbkk <ben@benworkandplay.com>
2019-04-17 18:46:52 +0700

    Production config

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const session = require('express-session')
 
 const app = express()
 
+const db = require('./config/database')
+
 // Load Routes
 const ideas = require('./routes/ideas')
 const users = require('./routes/users')
@@ -21,6 +23,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
+// Connect to Mongoose
 app.use(session({
   secret: 'secret',
   resave: true,
@@ -43,20 +46,17 @@ app.use((req, res, next) => {
 })
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb+srv://benbkk:b3NN@10120@cluster0-qsyd8.mongodb.net/vidjot', {
+mongoose.connect(db.mongoURI, {
   useNewUrlParser: true
 })
   .then(() => console.log('MongoDB is connected'))
   .catch(err => console.log(err))
-
-
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }))
 app.set('view engine', 'handlebars')
-
 
 // Index Route
 app.get('/', (req, res) => {
@@ -72,7 +72,7 @@ app.get('/about', (req, res) => {
 app.use('/ideas', ideas)
 app.use('/users', users)
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
